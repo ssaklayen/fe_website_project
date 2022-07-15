63aa6c8df8495c2b8a128597bd28cf10153d2461
Sabir Saklayen <sabirsaklayen83@gmail.com>
2022-07-15 14:46:34 -0600

    Removed compiler warnings (Minor misdemeanor)

--- frontend/src/components/infographic.component.jsx
+++ frontend/src/components/infographic.component.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 import batteryArray from "../images/urbanImage.png";
 
 export default function Infographic() {
