89e724e9d680c9169f5e3794cbfe00599014bbfa
Sabir Saklayen <sabirsaklayen83@gmail.com>
2022-07-22 10:33:50 -0600

    Removed a compiler warning, no change

--- frontend/src/components/contactform.component.jsx
+++ frontend/src/components/contactform.component.jsx
@@ -3,7 +3,6 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
 
 export default function ContactForm() {
   return (
