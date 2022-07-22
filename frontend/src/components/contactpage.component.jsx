4f432d76d16bce72d266703e221a70e857723521
Sabir Saklayen <sabirsaklayen83@gmail.com>
2022-07-22 11:34:12 -0600

    More small changes, flipped contact banner image, making adjustments to mobile responsiveness

--- frontend/src/components/contactpage.component.jsx
+++ frontend/src/components/contactpage.component.jsx
@@ -12,7 +12,7 @@ export default function ContactPage() {
         <ContactBanner />
         <Row>
           
-          <Col>
+          <Col id="contactInformation">
             <ContactInformation />
           </Col>
           
